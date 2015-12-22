37fce46c23c8ccff31e2b389f6812fad712aa14d
mcf171 <453372939@qq.com>
2015-12-22 18:17:07 +0800

    modify node structure
    modify node structure and add class edge

--- BestEffort.h
+++ BestEffort.h
@@ -5,7 +5,6 @@
 //  Created by 王珏 on 15/12/22.
 //  Copyright © 2015年 王珏. All rights reserved.
 //
-#pragma once
 
 #ifndef BestEffort_h
 #define BestEffort_h
@@ -14,11 +13,20 @@
 #include "Graph.h"
 #include "Query.h"
 #include "Node.h"
+#include <queue>
 #include <vector>
 
 using namespace std;
 
-double estInfUB(Node node, Graph g, double theta)
+class BestEffort{
+    
+public:
+    
+    priority_queue<Node> L;
+    
+};
+
+double estInfUB(Node& node, Graph g, double theta)
 {
     double maxInfluence;
     
@@ -29,15 +37,16 @@ double estInfUB(Node node, Graph g, double theta)
  * 离线部分主要功能计算每个点的最大影响上界
  */
 
-void bestEffortOffline(Graph g, double theta)
+void bestEffortOffline(Graph g, double theta, BestEffort& bestEffort)
 {
     
     vector<Node> nodes = g.nodes;
     
     for (int i = 0 ; i < nodes.size();  i ++) {
         //计算每个node的影响上界
-        double  maxInfluence = estInfUB(nodes[0], g, theta);
+        estInfUB(nodes[i], g, theta);
         //维护优先队列
+        bestEffort.L.push(nodes[i]);
     }
     
 }
@@ -60,6 +69,10 @@ void bestEffortOnline(Graph g ,Query q, double theta)
 vector<Node> bestEffort(Graph g, Query q, double theta)
 {
     vector<Node> node;
+    
+    BestEffort bestEffort;
+    
+    bestEffortOffline(g, theta, bestEffort);
     
     return node;
 }
